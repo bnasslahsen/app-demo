05e6e54e879e642c264bf7336fe1c9058c42da48
bnasslahsen <badr.nasslahsen@gmail.com>
2020-10-10 16:58:29 +0200

    Update Post.java

--- Post.java
+++ Post.java
@@ -7,12 +7,10 @@ class PostRouter {
 						.addMetadata("operation", new Operation().operationId("findByKey1AndKey2"))
 						.addMetadata("parameter", new Parameter().name("key1").description("My key1 description"))
 						.addMetadata("parameter", new Parameter().name("key2").description("My key2 description"))
-						.addMetadata("response", new ApiResponse().responseCode("200").description("This is my description").type(Post.class))
+						.addMetadata("response", new ApiResponse().responseCode("200").description("This is an operation description").type(Post.class))
 				, postHandler::all)
 				.andRoute(PUT("/posts/{id}")
-								.addMetadata("operation", new Operation().operationId("update")
-										.addParametersItem(new Parameter().name("id").in(ParameterIn.PATH.toString()).description("My key1 description"), new Parameter().name("key2").description("My key2 description"))
-										.responses(new ApiResponses().addApiResponse("202", new ApiResponse().description("This is my description"))))
+								.addMetadata("operation", new Operation().operationId("update").addParametersItem(new Parameter().name("id").in(ParameterIn.PATH.toString()).description("My id description")).responses(new ApiResponses().addApiResponse("202", new ApiResponse().description("This is another operation description"))))
 						, postHandler::update);
 	}
 }
