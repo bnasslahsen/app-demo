5ab02e17f65c16e48e29623df6b0f6b8effce483
bnasslahsen <badr.nasslahsen@gmail.com>
2020-10-10 18:05:34 +0200

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,21 +1,44 @@
-@Configuration
-class PostRouter {
 
-	@Bean
-	public RouterFunction<ServerResponse> routes(PostHandler postHandler) {
-		return route(GET("/posts1").and(queryParam("key1", "value1")).and(queryParam("key2", "value2"))
-						.addMetadata("operation", new Operation().operationId("findByKey1AndKey2"))
-						.addMetadata("parameter", new Parameter().name("key1").description("My key1 description"))
-						.addMetadata("parameter", new Parameter().name("key2").description("My key2 description"))
-						.addMetadata("response", new ApiResponse().responseCode("200").description("This is an operation description").type(Post.class))
-						.addMetadata("response", new ApiResponse().responseCode("404").description("item not found"))
-				, postHandler::all)
-				.andRoute(PUT("/posts/{id}")
-						.addMetadata("operation", new Operation().operationId("update")
-								.addParametersItem(new Parameter().name("id").in(ParameterIn.PATH.toString()).description("My id description"))
-								.responses(new ApiResponses()
-										.addApiResponse("202", new ApiResponse().description("This is another operation description"))
-										.addApiResponse("404", new ApiResponse().description("item to update, not found"))))
-				, postHandler::update);
+@Bean
+public RouterFunction<ServerResponse> routes(PostHandler postHandler) {
+	return route(GET("/posts").and(queryParam("key1", "value1")).and(queryParam("key2", "value2"))
+			.addMetadata("operation", new Operation().operationId("findPostsByKey1AndKey2"))
+			.addMetadata("parameter", new Parameter().name("key1").description("My key1 description"))
+			.addMetadata("parameter", new Parameter().name("key2").description("My key2 description"))
+		        .addMetadata("response", new ApiResponse().responseCode("200").description("This is an operation description").type(Post.class))
+			.addMetadata("response", new ApiResponse().responseCode("404").description("item not found"))
+		, postHandler::all)
+		.andRoute(PUT("/posts/{id}")
+			.addMetadata("operation", new Operation().operationId("updatePost")
+			.addParametersItem(new Parameter().name("id").in(ParameterIn.PATH.toString()).description("My id description"))
+			.responses(new ApiResponses().addApiResponse("202", new ApiResponse().description("OK"))
+			.addApiResponse("404", new ApiResponse().description("item to update, not found"))))
+		, postHandler::update);
 	}
+
+
+
+@Bean
+public RouterFunction<ServerResponse> routes(PostHandler postHandler) {
+	return route(GET("/posts").and(queryParam("key1", "value1")).and(queryParam("key2", "value2")).addMetadata("operation", findPostsByKey1AndKey2()), postHandler::all)
+			.andRoute(PUT("/posts/{id}").addMetadata("operation", updatePosts()), postHandler::update);
 }
+
+private Operation findPostsByKey1AndKey2() {
+	return new Operation().operationId("findPostsByKey1AndKey2")
+			.addParametersItem(new Parameter().name("key1").description("My key1 description"))
+			.addParametersItem(new Parameter().name("key2").description("My key2 description"))
+			.responses(new ApiResponses()
+					.addApiResponse("200", new ApiResponse().description("This is an operation description").type(Post.class))
+					.addApiResponse("404", new ApiResponse().description("item not found")))
+
+}
+
+private Operation updatePost() {
+	return new Operation().operationId("updatePost")
+			.addParametersItem(new Parameter().name("id").in(ParameterIn.PATH.toString()).description("My id description"))
+			.responses(new ApiResponses().addApiResponse("202", new ApiResponse().description("OK"))
+					.addApiResponse("404", new ApiResponse().description("item to update, not found")));
+
+}
+
